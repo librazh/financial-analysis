c8646037f885420a4aa1d21ef68a497eed658284
librazh <basel.zhang@gmail.com>
2018-08-19 22:03:26 +0800

    Fix the problem of getting retention ratio

--- equity_data.py
+++ equity_data.py
@@ -5,9 +5,12 @@ Note:
 """
 
 import logging
+
+from pandas import Period
 import pandas
 
 import tushare as ts
+
 
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
@@ -16,8 +19,7 @@ ts.set_token('6637031d14f4036a8dfffe9b186ff41164ddc1d2c9f6c5b521aaa56f')
 pro = ts.pro_api()
 
 
-def get_total_share(equity_code, report_period):    
-    global pro
+def get_total_share(equity_code, report_period):
     equity_data = pro.balancesheet(ts_code=equity_code, period=report_period)
     total_share = equity_data['total_share'][0]
     logger.debug('total_share=%s', total_share)
@@ -25,15 +27,13 @@ def get_total_share(equity_code, report_period):
 
 
 def get_net_income(equity_code, report_period):
-    global pro
-    equity_data = pro.income(ts_code=equity_code, report_period=report_period)
+    equity_data = pro.income(ts_code=equity_code, period=report_period)
     net_income = equity_data['n_income'][0]
     logger.debug('net_income=%s', net_income)
     return net_income
 
 
 def get_total_asset(equity_code, report_period):
-    global pro
     equity_data = pro.balancesheet(ts_code=equity_code, period=report_period)
     total_asset = equity_data['total_assets'][0]
     logger.debug('total_asset=%s', total_asset)
@@ -41,7 +41,6 @@ def get_total_asset(equity_code, report_period):
 
 
 def get_total_equity(equity_code, report_period):
-    global pro
     equity_data = pro.balancesheet(ts_code=equity_code, period=report_period)
     total_equity = equity_data['total_hldr_eqy_inc_min_int'][0]
     logger.debug('total_equity=%s', total_equity)
@@ -49,8 +48,7 @@ def get_total_equity(equity_code, report_period):
 
 
 def get_retained_earning(equity_code, report_period):
-    global pro
-    equity_data = pro.balancesheet(ts_code=equity_code, report_period=report_period)
+    equity_data = pro.balancesheet(ts_code=equity_code, period=report_period)
     retained_earning = equity_data['undistr_porfit'][0]
     logger.debug('retained_earning=%s', retained_earning)
     return retained_earning 
@@ -58,17 +56,33 @@ def get_retained_earning(equity_code, report_period):
     
 def get_retention_ratio(equity_code, report_period):
     
-    """
-    TODO: fix
-    """
+    equity_data = pandas.read_excel('dividend/' + equity_code + '.xlsx')
+    equity_data = equity_data.loc[equity_data[u'年度'] == str(Period(report_period).end_time.date()), [u'收益留存率']]
     
-    periods = pandas.period_range(end=report_period, freq='Y', periods=2)
-    logger.debug('periods[0]=%s', periods[0])
-    logger.debug('periods[1]=%s', periods[1])
-    retained_earning1 = get_retained_earning(equity_code, str(periods[0]))
-    retained_earning2 = get_retained_earning(equity_code, str(periods[1]))
+    retention_ratio = equity_data.iloc[0][0]
     
-    retained_earning = retained_earning1 - retained_earning2
+    logger.debug('retention_ratio=%s', retention_ratio)
+    return retention_ratio
+
+def get_dividend_per_share(equity_code, report_period):
+    equity_data = pandas.read_excel('dividend/' + equity_code + '.xlsx')
+    equity_data = equity_data.loc[equity_data[u'年度'] == str(Period(report_period).end_time.date()), [u'每股股利(元)']]
+    
+    dividend_per_share = equity_data.iloc[0][0]
+    
+    logger.debug('dividend_per_share=%s', dividend_per_share)
+    return dividend_per_share
+
+
+def get_retention_ratio_from_balance_sheet(equity_code, report_period):
+    
+    periods = pandas.period_range(end=report_period, freq='Y', periods=2).end_time.strftime('%Y%m%d')
+    logger.debug('periods=%s', periods)
+    
+    retained_earning_0 = get_retained_earning(equity_code, periods[0])
+    retained_earning_1 = get_retained_earning(equity_code, periods[1])
+    
+    retained_earning = retained_earning_1 - retained_earning_0
     
     net_income = get_net_income(equity_code, report_period)
     retention_ratio = retained_earning / net_income
@@ -77,5 +91,4 @@ def get_retention_ratio(equity_code, report_period):
 
 
 if __name__ == '__main__':
-    print(get_retained_earning('000876.SZ', '2017-12'))
-    print(get_retained_earning('000876.SZ', '2012-12'))
+    print(get_dividend_per_share('000876.SZ', '20171231'))
--- equity_valuator.py
+++ equity_valuator.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 
 import logging
-import pandas
 
 import equity_data
 
@@ -9,7 +8,7 @@ logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 
 
-def get_ggm_price(equity_code):
+def get_ggm_price(equity_code, report_period, required_return):
     """
     Gordon Growth Model (GGM). It is a special case of Dividend Discount Model (DDM).
     
@@ -21,12 +20,23 @@ def get_ggm_price(equity_code):
         g: growth rate
         r: required return
     """
+    d0 = equity_data.get_dividend_per_share(equity_code, report_period)
+    g = get_growth_rate(equity_code, report_period)
+    r = required_return
+    price = d0 * (1 + g) / (r - g)
+    return price
 
     
-def get_growth_rate(equity_code):
+def get_growth_rate(equity_code, report_period):
     """
     growth rate = ROE * retention ratio
     """
+    roe = get_roe(equity_code, report_period)
+    retention_ratio = equity_data.get_retention_ratio(equity_code, report_period)
+    growth_rate = roe * retention_ratio
+    
+    logger.debug('growth_rate=%s', growth_rate)
+    return growth_rate
 
 
 def get_roe(equity_code, report_period):
@@ -46,4 +56,5 @@ def get_roa(equity_code, report_period):
 
 
 if __name__ == '__main__':
-    print(get_roa('000876.SZ', '2017-12'))
+    print(get_ggm_price('000876.SZ', '20171231', 0.1))
+    print(get_ggm_price('000625.SZ', '20171231', 0.1))
